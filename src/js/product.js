49b59adc0c58e5e149903666aaf004431f5dbf39
Roberto Uriona <roberto.uriona@jalasoft.com>
2024-06-16 19:09:55 -0400

    fix issue related to Add Item to a cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Retrieve the current carts storred in an array from localStorage
+  let cart = getLocalStorage("so-cart") || [];
+  // Add the new product to the cart array
+  cart.push(product);
+  // Save the updated cart back to localStorage
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
