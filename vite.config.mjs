a9d342c4f0d7ed795f535a86a0253f06abe2d3cd
Roberto Uriona <roberto.uriona@jalasoft.com>
2024-06-20 16:37:30 -0400

    updating vite config file

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,3 +1,4 @@
+// vite.config.js
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
@@ -12,13 +13,19 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        product1: resolve(__dirname, "src/product_pages/cedar-ridge-rimrock-2.html"),
+        product1: resolve(
+          __dirname,
+          "src/product_pages/cedar-ridge-rimrock-2.html",
+        ),
         product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
-        product3: resolve(__dirname, "src/product_pages/northface-alpine-3.html"),
-        product4: resolve(__dirname, "src/product_pages/northface-talus-4.html"),
-        cartJs: resolve(__dirname, "src/js/cart.js"),
-        productJs: resolve(__dirname, "src/js/product.js"),
-        tentsJson: resolve(__dirname, "src/json/tents.json")
+        product3: resolve(
+          __dirname,
+          "src/product_pages/northface-alpine-3.html",
+        ),
+        product4: resolve(
+          __dirname,
+          "src/product_pages/northface-talus-4.html",
+        ),
       },
     },
   },
